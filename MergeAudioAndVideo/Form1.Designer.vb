9c34b76d76e30deedfb94a1d7606c8cca75e803a
Niten <Niten@NITEN-PC>
2024-02-07 02:21:13 +0800

    加入專案檔案。

---
+++ MergeAudioAndVideo/Form1.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(20, 22)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 26)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "選擇聲音(&A)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(20, 54)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(87, 26)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "選擇影像(&V)"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(113, 26)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(395, 22)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(113, 58)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(395, 22)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(113, 99)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(81, 39)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "合併(&M)"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Enabled = False
+        Me.Button4.Location = New System.Drawing.Point(233, 99)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(81, 39)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "取消(&C)"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'BackgroundWorker1
+        '
+        Me.BackgroundWorker1.WorkerReportsProgress = True
+        Me.BackgroundWorker1.WorkerSupportsCancellation = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(341, 99)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(122, 39)
+        Me.CheckBox1.TabIndex = 7
+        Me.CheckBox1.Text = "取消時刪除未完成檔案(&D)"
+        Me.CheckBox1.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Location = New System.Drawing.Point(0, 159)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(520, 150)
+        Me.TextBox3.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(520, 309)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "合併聲音和影像"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents TextBox3 As TextBox
+End Class
---
+++ MergeAudioAndVideo/Form1.vb
@@ -0,0 +1,169 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+
+Public Class Form1
+
+    Private Delegate Sub UpdataText(ByVal text As String)
+
+    Private Enum State
+        None = 0
+        Cancel
+        Finish
+    End Enum
+
+    Private Structure TempPath
+        Dim VideoPath As String
+        Dim AudioPath As String
+        Dim VideoFilename As String
+        Dim AudioFilename As String
+        Dim finishFileName As String
+    End Structure
+
+    Dim nowState As State
+    Dim tmpPath As TempPath
+
+    '選擇聲音
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = GetFilePath("選擇聲音檔案", "聲音檔案|*.mp4")
+    End Sub
+
+    '選擇影像
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox2.Text = GetFilePath("選擇影像檔案", "影像檔案|*.mp4")
+    End Sub
+
+    ''' <summary>
+    ''' 取得檔案路徑
+    ''' </summary>
+    ''' <param name="title">標題</param>
+    ''' <param name="filter">檔案類型</param>
+    ''' <returns>檔案路徑</returns>
+    Private Function GetFilePath(ByVal title As String, ByVal filter As String) As String
+        Dim result As String = String.Empty
+
+        Using openfile As New OpenFileDialog
+            openfile.Title = title
+            openfile.Filter = filter
+            If openfile.ShowDialog = DialogResult.OK Then
+                result = openfile.FileName
+            End If
+        End Using
+
+        Return result
+    End Function
+
+    '轉換
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        GetTempPath()
+        If IsReady() Then
+            Button3.Enabled = False
+            Button4.Enabled = True
+            nowState = State.None
+            BackgroundWorker1.RunWorkerAsync()
+        End If
+    End Sub
+
+    Private Sub GetTempPath()
+        tmpPath.VideoPath = System.IO.Path.GetDirectoryName(TextBox2.Text) & "\"
+        tmpPath.VideoFilename = System.IO.Path.GetFileName(TextBox2.Text)
+        tmpPath.AudioPath = System.IO.Path.GetDirectoryName(TextBox1.Text) & "\"
+        tmpPath.AudioFilename = System.IO.Path.GetFileName(TextBox1.Text)
+        tmpPath.finishFileName = System.IO.Path.GetFileNameWithoutExtension(tmpPath.VideoFilename) & "_Merge.mp4"
+    End Sub
+
+    ''' <summary>
+    ''' 確認已準備好相關檔案可開始合併
+    ''' </summary>
+    ''' <returns>沒準備好傳回 False ，否則傳回 True</returns>
+    Private Function IsReady() As Boolean
+        If Not My.Computer.FileSystem.FileExists(Application.StartupPath & "\ffmpeg.exe") Then
+            MsgBox("FFMPEG.exe 不存在")
+            Return False
+        End If
+
+        If Not My.Computer.FileSystem.FileExists(TextBox1.Text) Then
+            MsgBox("聲音檔不存在")
+            Return False
+        End If
+
+        If Not My.Computer.FileSystem.FileExists(TextBox2.Text) Then
+            MsgBox("影像檔不存在")
+            Return False
+        End If
+
+        If My.Computer.FileSystem.FileExists(tmpPath.finishFileName) Then
+            MsgBox(tmpPath.finishFileName & " 已經存在，請刪除或重新命名該檔案")
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        nowState = State.Cancel
+        If BackgroundWorker1.IsBusy Then BackgroundWorker1.CancelAsync()
+        Button3.Enabled = True
+        Button4.Enabled = False
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Merge()
+    End Sub
+
+    ''' <summary>
+    ''' 合併
+    ''' </summary>
+    Private Sub Merge()
+        Using ffmpeg As New Process
+            Dim procInfo As New ProcessStartInfo
+
+            procInfo.FileName = Application.StartupPath & "\ffmpeg.exe"
+            procInfo.Arguments = "-i """ & TextBox2.Text & """ -i """ & TextBox1.Text & """ -c:v copy -c:a aac " & tmpPath.finishFileName
+            procInfo.UseShellExecute = False
+            procInfo.WindowStyle = ProcessWindowStyle.Hidden
+            procInfo.RedirectStandardError = True
+            procInfo.CreateNoWindow = True
+
+            ffmpeg.StartInfo = procInfo
+            ffmpeg.Start()
+
+            Dim output As String
+
+            Using ffReader As StreamReader = ffmpeg.StandardError
+                Do
+                    If BackgroundWorker1.CancellationPending Then
+                        ffmpeg.Kill()
+                        Exit Do
+                    End If
+
+                    output = ffReader.ReadLine
+                    Debug.WriteLine(" >>> " & output)
+                    If Not output Is Nothing Then
+                        Me.Invoke(New UpdataText(AddressOf UpdataUI), output)
+                    End If
+
+                Loop Until ffmpeg.HasExited And output = Nothing Or output = ""
+            End Using
+        End Using
+    End Sub
+
+    Private Sub UpdataUI(ByVal text As String)
+        TextBox3.AppendText(text & vbNewLine)
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        If nowState = State.Cancel Then
+            MsgBox("合併取消")
+            If CheckBox1.Checked AndAlso My.Computer.FileSystem.FileExists(tmpPath.finishFileName) Then
+                My.Computer.FileSystem.DeleteFile(tmpPath.finishFileName)
+            End If
+        Else
+            MsgBox("合併完成")
+            Button3.Enabled = True
+            Button4.Enabled = False
+            TextBox1.Text = String.Empty
+            TextBox2.Text = String.Empty
+            nowState = State.None
+        End If
+    End Sub
+End Class
